465a3a9b86d1496c671326332bb75f37dadf6a75
rohan <rohansinghjadoan@gmail.com>
2025-08-30 20:54:21 +0530

    complete validation

--- crud_operation/models-val.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel,Field
-from typing import Optional
-class Employee(BaseModel):
-    id: int= Field(...,gt=0,title="Employee ID",description="Unique identifier for the employee")
-    name: str= Field(...,min_length=3,max_length=30,title="Employee Name",description="Full name of the employee",regex="^[a-zA-Z ]+$")
-    department:str=Field(...,min_length=3,max_length=30)
-    age: Optional[int]
