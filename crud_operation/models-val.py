706be2fa32afdf429b1c4b1fb63bcd1c521319e4
rohan <rohansinghjadoan@gmail.com>
2025-08-30 20:37:03 +0530

    add optinal value

--- crud_operation/models-val.py
+++ crud_operation/models-val.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel,Field
+from typing import Optional
 class Employee(BaseModel):
     id: int= Field(...,gt=0,title="Employee ID",description="Unique identifier for the employee")
     name: str= Field(...,min_length=3,max_length=30,title="Employee Name",description="Full name of the employee",regex="^[a-zA-Z ]+$")
     department:str=Field(...,min_length=3,max_length=30)
-    age: int= Field(...,lt=120)
+    age: Optional[int]
