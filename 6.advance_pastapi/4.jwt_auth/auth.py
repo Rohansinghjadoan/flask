49b21d3b0a36838b729674a92a7ac83d7bcd8bda
rohan <rohansinghjadoan@gmail.com>
2025-09-23 00:03:21 +0530

    main.py

--- 6.advance_pastapi/4.jwt_auth/auth.py
+++ 6.advance_pastapi/4.jwt_auth/auth.py
@@ -1,30 +1,29 @@
 from datetime import datetime, timedelta, timezone
-from authlib.jose import JWTError, jwt
-from fastapi import Depends, HTTPException, status
-from authlib.jose.errors import JoseError
- 
+from authlib.jose import JoseError, jwt
+from fastapi import HTTPException
 
 # constants
+SECRET_KEY = 'my_secret'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRY_MINUTES = 30
 
-SECRET_KEY='my_secret'
-AGALGORITHM='HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES=30
 
 # functions
-def create_access_token(data:dict):
-    header={"alg":AGALGORITHM}
-    expire=datetime.now(timezone.utc)+timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
-    payload=data.copy()
-    payload.update({"exp":expire})
-    return jwt.encode(header,payload,SECRET_KEY).decode('utf-8')
+def create_access_token(data: dict):
+    header = {'alg': ALGORITHM}
+    expire = datetime.now(timezone.utc) + timedelta(ACCESS_TOKEN_EXPIRY_MINUTES)
+    payload = data.copy()
+    payload.update({'exp': expire})
+    return jwt.encode(header, payload, SECRET_KEY).decode('utf-8')
 
-def verify_token(token:str):
+
+def verify_token(token: str):
     try:
-        claims=jwt.decode(token,SECRET_KEY)
-         
-        username=claims.get('sub')
+        claims = jwt.decode(token, SECRET_KEY)
+        claims.validate()
+        username = claims.get('sub')
         if username is None:
-            raise HTTPException(status_code=401,detail='could not validate credentials')
-        return {'username':username} 
+            raise HTTPException(status_code=401, detail='Token missing')
+        return username
     except JoseError:
-        raise HTTPException(status_code=401,detail='could not validate credentials')
+        raise HTTPException(status_code=401, detail="Couldn't Validate Credentials")
--- 6.advance_pastapi/4.jwt_auth/utils.py
+++ 6.advance_pastapi/4.jwt_auth/utils.py
@@ -1,19 +1,19 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-fake_user_db={
-    'johndoe':{
-        'username':'johndoe',
-        'hashed_password':pwd_context.hash('secret123')
-
-
+fake_user_db = {
+    'johndoe': {
+        'username': 'johndoe',
+        'hashed_password': pwd_context.hash('secret123')
     }
 }
 
-def get_user(db,username:str):
-    user=fake_user_db.get(username)
+
+def get_user(username: str):
+    user = fake_user_db.get(username)
     return user
 
-def verify_password(plain_password,hashed_password):
-    return pwd_context.verify(plain_password,hashed_password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
